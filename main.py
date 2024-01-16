d4ee1dab15456292ed1943333d1c88a7c12b72d2
swongkc13 <141418651+simonwongrw@users.noreply.github.com>
2024-01-16 11:43:12 +0100

    Updated console print

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
-cap = cv2
+cap = cv2.VideoCapture(0)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img, flipType=False)
+
+    if hands:
+        print("Hand detected")
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
