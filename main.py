d6673f33c27bbe3ab1301633a629146705f4cb55
swongkc13 <swongkc13@gmail.com>
2024-01-16 11:57:34 +0100

    Window closes after hand is shown for 5 seconds

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import time
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
+start_time = time.time()
 
 while True:
     success, img = cap.read()
@@ -11,5 +13,13 @@ while True:
     if hands:
         print("Hand detected")
 
+    elapsed_time = time.time() - start_time
+    if elapsed_time >= 3:
+        print("Camera Activated")
+        break
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+cap.release()
+cv2.destroyAllWindows()
